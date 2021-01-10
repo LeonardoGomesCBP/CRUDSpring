bee2bfe86847c0eb52cb19ed147708a773b2b44a
Leonardo <leonardogomescbp@gmail.com>
2021-01-10 06:37:18 -0300

    delete, edit departamento
    funções de deletar e editar do departamento feitas

--- src/main/java/com/leonardo/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/leonardo/curso/boot/web/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,4 +34,32 @@ public class DepartamentoController {
 		service.salvar(departamento);
 		return "redirect:/departamentos/cadastrar";
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		service.editar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		
+		if (service.depertamentoTemCargos(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargo(s) vinculado(s).");
+		} else {
+			service.excluir(id);
+			model.addAttribute("success", "Departamento excluído com sucesso.");
+		}
+		
+		return listar(model);
+	}
+	
+	
+	
 }
